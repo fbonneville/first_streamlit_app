0d3a72b86df36e7eba90e481bdebebe807bf6011
fbonneville <66326601+fbonneville@users.noreply.github.com>
2023-06-07 20:13:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,5 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach, and Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled free range Eggs')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
