df2a82d8a611bca853732d93d2e4994f0d191d3e
fbonneville <66326601+fbonneville@users.noreply.github.com>
2023-06-07 20:11:50 -0400

    Update streamlit_app.py - Adding menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
-streamlit.title('My parents new healthy Diner')
+streamlit.title('My parents new Healthy Diner')
+
+streamlit.header ('Breakfast Menu')
+
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach, and Rocket Smoothie')
+streamlit.text('Hard-Boiled free range Eggs')
+streamlit.text('Mud')
